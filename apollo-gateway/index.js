3d0408243454e01c4806cbdd0b73c36eb97a3a45
Ryo Wang <minovfalk@gmail.com>
2022-05-05 14:36:29 +0700

    updated server.go, added apollo landing page plugin for production

--- apollo-gateway/index.js
+++ apollo-gateway/index.js
@@ -2,14 +2,26 @@
 require('dotenv').config()
 
 const { ApolloServer } = require('apollo-server');
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloGateway } = require('@apollo/gateway');
+const { ApolloServerPluginLandingPageLocalDefault,
+    ApolloServerPluginLandingPageProductionDefault
+} = require('apollo-server-core');
 
 
 const gateway = new ApolloGateway();
 
 const server = new ApolloServer({
     gateway,
-    introspection: process.env.NODE_ENV !== 'prod'
+    introspection: process.env.NODE_ENV !== 'prod',
+    plugins: [
+        // Install a landing page plugin based on NODE_ENV
+        process.env.NODE_ENV === 'prod'
+          ? ApolloServerPluginLandingPageProductionDefault({
+              graphRef: "my-graph-id@my-graph-variant",
+              footer: false,
+            })
+          : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+      ],
 });
 
 server.listen().then(({ url }) => {
